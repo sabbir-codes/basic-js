d362ea039c602a8627a309d2a44ea5acd68e4b9e
sabbiraiml <sabbir.ah799@gmail.com>
2026-07-14 15:42:07 +0600

    function, array, object-Basics

--- script.js
+++ script.js
@@ -1,13 +1,82 @@
 // loop
 
-let i = 1;
+// let i = 1;
 
-while (i <= 5) {
-    console.log(i);
-    i++;
-}
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
 
-for(let i=1; i<=5; i++){
-    console.log(i);
+// for(let i=1; i<=5; i++){
+//     console.log(i);
     
+// }
+
+// FUNCTION
+// function greet(){
+// console.log("Good Morning");
+// }
+
+// greet();
+
+
+// function add(a, b){
+//     return(a + b)
+// }
+
+//  let result= add(5, 7);
+//  console.log(result);
+
+// //  another way
+
+//  let a = 5;
+//  let b = 7;
+// function add(a, b){
+//      console.log(a + b);
+// }
+
+// add(a, b);
+ 
+// ARRAY
+// let students=["Sabbir", "Sabuj", "Arman", "Raju"];
+// console.log(students[3]);
+// students.push("Hasan")
+// console.log(students);
+// students.pop()
+// console.log(students);
+// console.log(students.length);
+
+// for(let i=0; i<students.length; i++){
+//     console.log(students[i]);  
+// }
+
+// let student = {
+//     name: "Sabbir",
+//     age: 22,
+//     city: "Dhaka"
+// };
+ 
+// console.log(student.name);
+// console.log(student["city"]);
+// console.log(student["age"]);
+// student.push(city : "Chattogram");
+
+// ALL IN ONE 
+const students = [
+  {
+    name: "Sabbir",
+    marks: 85
+  },
+  {
+    name: "Rahim",
+    marks: 72
+  }
+];
+
+for (let i = 0; i < students.length; i++) {
+  if (students[i].marks >= 80) {
+    console.log(students[i].name + " passed with an A grade.");
+  } else {
+    console.log(students[i].name + " needs improvement.");
+  }
 }
